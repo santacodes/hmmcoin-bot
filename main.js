f48865612a2c5b1ba74888cebac6d95f2c323104
Santhosh <52504160+santacodes@users.noreply.github.com>
2022-10-28 22:33:55 +0530

    intermediate change to ensure version safety

--- main.js
+++ main.js
@@ -7,21 +7,36 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
   const botid = client.user.id;
   const hmmGuilds = client.guilds.cache.map((guild) => guild.id);
-  console.log(hmmGuilds);
+  console.log("hmmguilds = "+hmmGuilds);
+  console.log("members of guilds "+ hmmGuilds.members)
   let price = String(getprices());
+  let member = client.user.id;
+  member.setNickname(price));
+  fetchAllGuildMembers()
   //client.user.setPresence({ activities: [{ name: 'Getting the Price of HMM' }], status: 'idle' });
-});
-for (var i = 0; i < length(hmmGuilds); i++) {
-  const mem = hmmGuilds[i].members;
-  for (j = 0; j < Number(mem.length); i++) {
-    if (mem[j] === client.id) {
-      mem[j].setNickname("Test Nickname");
-    }
-  }
-}
+  //const bot_username = client.user.tag;
+  //let member = client.guild.members.getcache
 //function changenickname() {
 
 //}
+
+const fetchAllGuildMembers = () => {
+  members = hmmGuilds.members;
+  console.log("These are the members "+members)
+  getfetch();
+  let guilds = client.guilds.fetch(819572193796292678);
+  
+  console.log("this is from fetchall"+guilds);
+  for (let i = 0; i < guilds.length; i += 1) {
+  const guild = bot.guilds.cache.get(guilds[i].id); guild.members.fetch(); console.log("Guild members fetched for - ${guilds[i].name}(${guilds[i].id})")
+  };
+};
+
+async function getfetch(){
+  let gg = await client.guilds.fetch(819572193796292678);
+  console.log("this is from getfetch"+gg);
+  return gg.toArray();
+}
 
 client.on("message", (msg) => {
   console.log(msg);
