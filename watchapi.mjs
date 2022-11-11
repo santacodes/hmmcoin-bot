b77b1f41c2ca9cae4f1e114e95e675bd083fef52
Santhosh <52504160+santacodes@users.noreply.github.com>
2022-11-11 23:11:02 +0530

    made bug fixes to watchapi

--- watchapi.mjs
+++ watchapi.mjs
@@ -5,10 +5,9 @@ import fetch from "node-fetch";
 const botmsg = "[HMC] ";
 
 export default function contents() {
-    let price = null;
     let fileText = readFileSync('./data.json');
     let jsonParsed = JSON.parse(fileText);
-    price = jsonParsed["price"];
+    let price = jsonParsed["price"];
 
     fetch("https://nomics.com/_next/data/2ztKxC_zLgSwCcRmO7GeP/assets/hmc2-hmmcoin.json?currency=hmc2-hmmcoin", {
     method: "GET",
@@ -17,12 +16,13 @@ export default function contents() {
     },
   })
     .then((response) => response.json())
-    .then((response) => response["pageProps"]["data"]["ticker"]["1d"]["price_change_pct"])
+    .then((response) => { if(response["pageProps"]["data"]["ticker"]["1d"]["price_change_pct"] != undefined) return response; else return null; })
+    .catch((error) => {return null;})
     .then((response) => console.log(JSON.stringify(response, null, "\t")))
-    .then((response) => {writeFile("./data.json", JSON.stringify({"price":price,"24h":response}, null, 2), (err) => {
+    .then((response) => {if(response != null){writeFile("./data.json", JSON.stringify([{"data":response}], null, 2), (err) => {
         if (err) { console.error(botmsg + err); };
         console.log(botmsg + "Updated the 24h percentage in JSON file");
-    });});
+    });}});
 }
 
 contents(); //for testing (only runs when this file is called)
